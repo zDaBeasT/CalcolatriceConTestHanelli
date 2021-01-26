58af6abfe8e02b21b8ddcfa9492b033d1d1ea9fb
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2021-01-26 13:04:02 +0100

    Update the program

--- Operazioni/Gestione.cs
+++ Operazioni/Gestione.cs
@@ -7,8 +7,30 @@ namespace Operazioni
         public static int Somma(int a, int b)
         {
 
-            int somma = a + b;
-            return somma;
+            return a+b;
+        }
+
+        public static int Differenza(int a, int b)
+        {
+
+            return a - b;
+        }
+
+        public static double SommaDecimale(double a, double b)
+        {
+
+            return a + b;
+        }
+
+        public static int Prodotto(int a, int b)
+        {
+
+            return a * b;
+        }
+
+        public static int Quoziente(int a, int b)
+        {
+            return a / b;
         }
     }
 }
--- OperazioniTest/GestioneTest.cs
+++ OperazioniTest/GestioneTest.cs
@@ -49,5 +49,49 @@ namespace OperazioniTest
             somma_effettiva = Gestione.Somma(a, b);
             Assert.AreEqual(somma_aspettata, somma_effettiva);
         }
+
+        [TestMethod]
+        public void TestDifferenzaPositivi()
+        {
+            int a = 9;
+            int b = 7;
+            int differenza_aspettata = 2;
+            int differenza_effettiva;
+            differenza_effettiva = Gestione.Differenza(a, b);
+            Assert.AreEqual(differenza_aspettata, differenza_effettiva);
+        }
+
+        [TestMethod]
+        public void TestSommaDecimalePositivi()
+        {
+            double a = 3.5;
+            double b = 6.5;
+            double somma_aspettata = 10;
+            double somma_effettiva;
+            somma_effettiva = Gestione.SommaDecimale(a, b);
+            Assert.AreEqual(somma_aspettata, somma_effettiva);
+        }
+
+        [TestMethod]
+        public void TestProdottoPositivi()
+        {
+            int a = 3;
+            int b = 5;
+            int prodotto_aspettato = 15;
+            int prodotto_effettivo;
+            prodotto_effettivo = Gestione.Prodotto(a, b);
+            Assert.AreEqual(prodotto_aspettato, prodotto_effettivo);
+        }
+
+        [TestMethod]
+        public void TestQuozientePositivi()
+        {
+            int a = 10;
+            int b = 2;
+            int quoziente_aspettato = 5;
+            int quoziente_effettivo;
+            quoziente_effettivo = Gestione.Quoziente(a, b);
+            Assert.AreEqual(quoziente_aspettato, quoziente_effettivo);
+        }
     }
 }
